50d6e77e0f50dfec65db6b623ee9b0cb6c152ba1
caiboqiang <908507670@qq.com>
2019-08-01 19:29:48 +0800

    添加实体类模块

---
+++ entity/src/main/java/com/cai/EntitleApplication.java
@@ -0,0 +1,13 @@
+package com.cai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntitleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntitleApplication.class, args);
+    }
+
+}
