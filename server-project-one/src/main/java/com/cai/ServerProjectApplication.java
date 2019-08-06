a1e7b3e9f6ea287dbc665a6734d63194b5eee849
caiboqiang <908507670@qq.com>
2019-08-06 18:02:07 +0800

    调试分布式事物测试通过RS1

--- server-project-one/src/main/java/com/cai/ServerProjectApplication.java
+++ server-project-one/src/main/java/com/cai/ServerProjectApplication.java
@@ -1,12 +1,13 @@
 package com.cai;
 
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient //TODO 客户端
-//@EnableDistributedTransaction
+@EnableDistributedTransaction
 public class ServerProjectApplication {
 
     public static void main(String[] args) {
