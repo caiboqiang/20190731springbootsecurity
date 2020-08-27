6fd6ed1a82816b56b6dc8d7a396d839cb0464b3e
908507670@qq.com <908507670@qq.com>
2020-08-27 23:20:17 +0800

    6035

--- server-project-user-6035/src/main/java/com/cai/serverResouce/OAuth2ServerConfig.java
+++ server-project-user-6035/src/main/java/com/cai/serverResouce/OAuth2ServerConfig.java
@@ -1,34 +1,34 @@
 package com.cai.serverResouce;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-
-@Configuration
-@EnableResourceServer //表示这个服务作为资源服务器的一个服务
-public class OAuth2ServerConfig extends ResourceServerConfigurerAdapter {
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resourceServerSecurityConfigurer) throws Exception {
-        resourceServerSecurityConfigurer.resourceId("server-user"); //配置id 资源服务器id
-    }
-
-    /**
-     * 权限检查
-     * @param httpSecurity
-     * @throws Exception
-     */
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests ()
-                .antMatchers ( HttpMethod.POST ).access ( "#oauth2.hasScope('write')" )//post 请求都要验证是否有这个权限
-                .antMatchers ( HttpMethod.GET ).access ("#oauth2.hasScope('read')");
-//        httpSecurity.authorizeRequests ().
-//                antMatchers ( "/oauth/chech_token" ).
-//                permitAll ().
-//                anyRequest ().authenticated ();//配置不需要拦截的请求
-
-    }
-}
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+//
+////@Configuration
+////@EnableResourceServer //表示这个服务作为资源服务器的一个服务
+//public class OAuth2ServerConfig extends ResourceServerConfigurerAdapter {
+//    @Override
+//    public void configure(ResourceServerSecurityConfigurer resourceServerSecurityConfigurer) throws Exception {
+//        resourceServerSecurityConfigurer.resourceId("server-user"); //配置id 资源服务器id
+//    }
+//
+//    /**
+//     * 权限检查
+//     * @param httpSecurity
+//     * @throws Exception
+//     */
+//    @Override
+//    public void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authorizeRequests ()
+//                .antMatchers ( HttpMethod.POST ).access ( "#oauth2.hasScope('write')" )//post 请求都要验证是否有这个权限
+//                .antMatchers ( HttpMethod.GET ).access ("#oauth2.hasScope('read')");
+////        httpSecurity.authorizeRequests ().
+////                antMatchers ( "/oauth/chech_token" ).
+////                permitAll ().
+////                anyRequest ().authenticated ();//配置不需要拦截的请求
+//
+//    }
+//}
--- server-project-user-6035/src/main/java/com/cai/serverResouce/OAuth2WebSecurityConfig.java
+++ server-project-user-6035/src/main/java/com/cai/serverResouce/OAuth2WebSecurityConfig.java
@@ -1,66 +1,66 @@
 package com.cai.serverResouce;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
-import org.springframework.security.oauth2.provider.token.*;
-import org.springframework.stereotype.Component;
-
-/**
- * 验证token是不是合法的
- */
-@Configuration
-@EnableWebSecurity
-public class OAuth2WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    /**
-     * 需要远程验证
-     */
-    @Bean
-    public ResourceServerTokenServices tokenServices(){
-        //这是一个调用远程服务的
-        RemoteTokenServices tokenServices = new RemoteTokenServices ( );
-        tokenServices.setClientId ("adminApp");
-        tokenServices.setClientSecret (passwordEncoder().encode ("12345"));
-        tokenServices.setCheckTokenEndpointUrl ("http://localhost:5056/oauth/chech_token");//校验的地址
-        //userData
-        tokenServices.setAccessTokenConverter (getAccessTokenConverter()  ); //把token转换成用户数据
-        return tokenServices;
-    }
-    //获取用户数据
-    private AccessTokenConverter getAccessTokenConverter(){
-        DefaultAccessTokenConverter accessTokenConverter = new DefaultAccessTokenConverter();
-        //为了设置userDetailsService 转换
-        DefaultUserAuthenticationConverter userTokenConverter = new DefaultUserAuthenticationConverter();
-        userTokenConverter.setUserDetailsService (userDetailsService);
-
-        accessTokenConverter.setUserTokenConverter (userTokenConverter);
-        return accessTokenConverter;
-    }
-
-    /**
-     * 把它声明成一个bean
-     * @return
-     * @throws Exception
-     */
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        OAuth2AuthenticationManager auth2AuthenticationManager = new OAuth2AuthenticationManager();
-        auth2AuthenticationManager.setTokenServices (tokenServices());
-        return auth2AuthenticationManager;
-    }
-}
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
+//import org.springframework.security.oauth2.provider.token.*;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * 验证token是不是合法的
+// */
+////@Configuration
+////@EnableWebSecurity
+//public class OAuth2WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//    /**
+//     * 需要远程验证
+//     */
+//    @Bean
+//    public ResourceServerTokenServices tokenServices(){
+//        //这是一个调用远程服务的
+//        RemoteTokenServices tokenServices = new RemoteTokenServices ( );
+//        tokenServices.setClientId ("adminApp");
+//        tokenServices.setClientSecret (passwordEncoder().encode ("12345"));
+//        tokenServices.setCheckTokenEndpointUrl ("http://localhost:5056/oauth/chech_token");//校验的地址
+//        //userData
+//        tokenServices.setAccessTokenConverter (getAccessTokenConverter()  ); //把token转换成用户数据
+//        return tokenServices;
+//    }
+//    //获取用户数据
+//    private AccessTokenConverter getAccessTokenConverter(){
+//        DefaultAccessTokenConverter accessTokenConverter = new DefaultAccessTokenConverter();
+//        //为了设置userDetailsService 转换
+//        DefaultUserAuthenticationConverter userTokenConverter = new DefaultUserAuthenticationConverter();
+//        userTokenConverter.setUserDetailsService (userDetailsService);
+//
+//        accessTokenConverter.setUserTokenConverter (userTokenConverter);
+//        return accessTokenConverter;
+//    }
+//
+//    /**
+//     * 把它声明成一个bean
+//     * @return
+//     * @throws Exception
+//     */
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        OAuth2AuthenticationManager auth2AuthenticationManager = new OAuth2AuthenticationManager();
+//        auth2AuthenticationManager.setTokenServices (tokenServices());
+//        return auth2AuthenticationManager;
+//    }
+//}
